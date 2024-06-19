ab584bcc8097451fc0d05fdfd8e5c7ba8fc0a082
LICHIN.C <lichinchandran1432gmail.com>
2024-06-19 01:03:53 -0700

    cors

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ import courtRouter from './routes/courtRouter.js';
 
 
 connectDB()
-const  app=express()
-app.use(cors({ origin: 'http://localhost:5173' }));
-  app.use(express.json());
+const app = express()
+// https://turfshub.netlify.app/
+// app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://turfshub.netlify.app/' }));
+
+app.use(express.json());
 
 app.use('/', userRoutes);
 app.use('/admin', adminRouter);
-app.use('/court',courtRouter)
+app.use('/court', courtRouter)
 
-app.listen(process.env.port,()=>{
-    console.log(`Server is running on http://localhost:${process.env.port}`);
+app.listen(process.env.port, () => {
+  console.log(`Server is running on http://localhost:${process.env.port}`);
 })
 
