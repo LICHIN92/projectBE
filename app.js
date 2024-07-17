b1b6cd8883880f72d1be1086ca754012e555b608
LICHIN.C <lichinchandran1432gmail.com>
2024-07-17 13:10:20 -0700

    neworigin

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express()
 
 const allowedOrigins = [
   'https://project-fe-gilt.vercel.app',
-  'https://turfhub-lichin-chandrans-projects.vercel.app/'
+  'https://turfhub-lichin-chandrans-projects.vercel.app/',
   'http://localhost:5173',
   'https://turfhub.netlify.app'
 ];
